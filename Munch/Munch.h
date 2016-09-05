f0440fa0b6f6b0e9e04eb0281eb32aaccacd9e55
mrahmiao <mrahmiao@gmail.com>
2016-09-05 11:17:05 +0800

    Initial Commit
    Signed-off-by: mrahmiao <mrahmiao@gmail.com>

---
+++ Munch/Munch.h
@@ -0,0 +1,19 @@
+//
+//  Munch.h
+//  Munch
+//
+//  Created by mrahmiao on 9/5/16.
+//  Copyright © 2016 NSTweak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Munch.
+FOUNDATION_EXPORT double MunchVersionNumber;
+
+//! Project version string for Munch.
+FOUNDATION_EXPORT const unsigned char MunchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Munch/PublicHeader.h>
+
+
---
+++ README.md
@@ -0,0 +1,7 @@
+# Munch
+
+Munch is a Swift library that simplifies Core Graphics drawing operations.
+
+## License
+
+Munch is available under the MIT license. See the LICENSE file for more info.
