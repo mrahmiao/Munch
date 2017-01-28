041062a6b23548bc05d6e10d0ceea71d3ec4c1ea
mrahmiao <mrahmiao@gmail.com>
2017-01-28 22:44:29 +0800

    feat: Add multiple methods
    Signed-off-by: mrahmiao <mrahmiao@gmail.com>

--- Source/Munch.swift
+++ Source/Munch.swift
@@ -51,4 +51,82 @@ extension CGPoint {
   public static func / (point: CGPoint, scalar: CGFloat) -> CGPoint {
     return CGPoint(x: point.x / scalar, y: point.y / scalar)
   }
+
+  public func distance(to point: CGPoint) -> CGFloat {
+    return sqrt(pow(x.distance(to: point.x), 2) + pow(y.distance(to: point.y), 2))
+  }
+
+  public func rounded() -> CGPoint {
+    return CGPoint(x: x.rounded(), y: y.rounded())
+  }
+
+  public func halfRounded() -> CGPoint {
+    return CGPoint(x: (x * 2).rounded() / 2, y: (y * 2).rounded() / 2)
+  }
+}
+
+// MARK: - CGRect
+extension CGRect {
+  public var center: CGPoint {
+    return CGPoint(x: midX, y: midY)
+  }
+
+  public init(center: CGPoint, size: CGSize) {
+    self.size = size
+    self.origin = CGPoint(x: center.x - size.width / 2, y: center.y - size.height / 2)
+  }
+
+  public init(squareCenter: CGPoint, sideLength: CGFloat) {
+    self.init(center: squareCenter, size: CGSize(width: sideLength * 2, height: sideLength * 2))
+  }
+
+  public func rect(byFittingIn destRect: CGRect) -> CGRect {
+    let aspect = size.aspectScale(fitIn: destRect)
+    let targetSize = size * aspect
+    return CGRect(center: destRect.center, size: targetSize)
+  }
+
+  public func rect(byFillingIn destRect: CGRect) -> CGRect {
+    let aspect = size.aspectScale(fillIn: destRect)
+    let targetSize = size * aspect
+    return CGRect(center: destRect.center, size: targetSize)
+  }
+
+  public func rounded() -> CGRect {
+    return CGRect(origin: origin.rounded(), size: size.rounded())
+  }
+}
+
+// MARK: - CGSize
+extension CGSize {
+  public static func * (aSize: CGSize, factor: CGFloat) -> CGSize {
+    return CGSize(width: aSize.width * factor, height: aSize.height * factor)
+  }
+
+  public static func / (aSize: CGSize, scalar: CGFloat) -> CGSize {
+    return CGSize(width: aSize.width / scalar, height: aSize.height / scalar)
+  }
+
+  public static func + (aSize: CGSize, scalar: CGFloat) -> CGSize {
+    return CGSize(width: aSize.width + scalar, height: aSize.height + scalar)
+  }
+
+  private func scales(with rect: CGRect) -> (CGFloat, CGFloat) {
+    let size = rect.size
+    return (size.width / width, size.height / height)
+  }
+
+  public func aspectScale(fitIn rect: CGRect) -> CGFloat {
+    let (scaleW, scaleH) = scales(with: rect)
+    return min(scaleW, scaleH)
+  }
+
+  public func aspectScale(fillIn rect: CGRect) -> CGFloat {
+    let (scaleW, scaleH) = scales(with: rect)
+    return max(scaleW, scaleH)
+  }
+
+  public func rounded() -> CGSize {
+    return CGSize(width: width.rounded(), height: height.rounded())
+  }
 }
